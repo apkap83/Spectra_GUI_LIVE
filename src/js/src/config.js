1516dc8152e93ceeb7d86d6823f89abf27c8c378
apka83 <ap.kapetanios@gmail>
2023-12-08 18:29:08 +0200

    First Commit For this Branch

--- src/js/src/config.js
+++ src/js/src/config.js
@@ -1,9 +1,9 @@
 const config = {
   appTitle: "Spectra Live",
-  // apiPrefix: "https://10.10.18.120:4000",
-  // apiPrefixForIframeDuringRemoteDev: "https://10.10.18.120:4000",
-  apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
-  apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
+  apiPrefix: "http://127.0.0.1:15000",
+  apiPrefixForIframeDuringRemoteDev: "https://10.10.18.120:4000",
+  // apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
+  // apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
   jwtTokenKeyName: "JWT_Token",
 };
 
