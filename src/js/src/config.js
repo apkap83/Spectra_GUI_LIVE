21cb6a8c7b9f52166f08a903b114f0801ac50934
apka83 <ap.kapetanios@gmail>
2023-12-30 21:31:23 +0200

    OK

--- src/js/src/config.js
+++ src/js/src/config.js
@@ -1,14 +1,14 @@
 const config = {
   appTitle: "Spectra Live",
   // DEV
-  apiPrefix: "http://127.0.0.1:4000",
-  apiPrefixForIframeDuringRemoteDev: "http://127.0.0.1:4000",
-  homePage: "/nova/allspectraincidents",
+  // apiPrefix: "http://127.0.0.1:4000",
+  // apiPrefixForIframeDuringRemoteDev: "http://127.0.0.1:4000",
+  // homePage: "/nova/allspectraincidents",
 
   // PRODUCTION
-  // apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
-  // apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
-  // homePage: "/nova/allspectraincidents"
+  apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
+  apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
+  homePage: "/nova/allspectraincidents",
 
   jwtTokenKeyName: "JWT_Token",
   sessionStorageKey: "Session",
