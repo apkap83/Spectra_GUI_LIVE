c3371420d09fabef6d6877c02807de5a86b522db
Apostolos <ap.kapetanios@gmail.com>
2024-01-10 22:34:57 +0200

    Seems OK

--- src/js/src/config.js
+++ src/js/src/config.js
@@ -1,16 +1,16 @@
 const config = {
   appTitle: "Spectra Live",
   // DEV
-  apiPrefix: "http://127.0.0.1:4000",
-  apiPrefixForIframeDuringRemoteDev: "http://127.0.0.1:4000",
-  apiPrefixForOpenAIFunctionsPage: "http://127.0.0.1:4000",
-  homePage: "/nova/allspectraincidents",
+  // apiPrefix: "http://127.0.0.1:4000",
+  // apiPrefixForIframeDuringRemoteDev: "http://127.0.0.1:4000",
+  // apiPrefixForOpenAIFunctionsPage: "http://127.0.0.1:4000",
+  // homePage: "/nova/allspectraincidents",
 
   // PRODUCTION
-  // apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
-  // apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
-  // apiPrefixForOpenAIFunctionsPage: `https://${window.location.hostname}:${window.location.port}`,
-  // homePage: "/nova/allspectraincidents",
+  apiPrefix: `https://${window.location.hostname}:${window.location.port}`,
+  apiPrefixForIframeDuringRemoteDev: `https://${window.location.hostname}:${window.location.port}`,
+  apiPrefixForOpenAIFunctionsPage: `https://${window.location.hostname}:${window.location.port}`,
+  homePage: "/nova/allspectraincidents",
 
   jwtTokenKeyName: "JWT_Token",
   sessionStorageKey: "Session",
